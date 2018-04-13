bf72aeeaf898193c527e76681a789db447325b05
ct <ct@ctdeMac.local>
2018-04-13 09:30:22 +0800

    implement admin/resume

---
+++ app/controllers/admin/resumes_controller.rb
@@ -0,0 +1,13 @@
+class Admin::ResumesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_is_admin
+
+  layout "admin"
+
+  def index
+    @job = Job.find(params[:job_id])
+    @resumes = @job.resumes.recent.paginate(page: params[:page], per_page: 5)
+  end
+
+
+end
