50a071e9bb3c3e16e6bc15e44a85c43f81884b57
ct <ct@ctdeMac.local>
2018-04-18 18:56:29 +0800

    fixed account/resume & favorites view & set seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "创建public jobs"
+
+create_jobs = for i in ["技术", "财务", "设计", "运营", "市场", "销售", "客服"] do
+  for j in ["北京", "上海", "深圳", "广州", "杭州", "成都", "香港", "海外"] do
+    Job.create!([name: "Job", content: "test ",
+      wage_upper_bound: rand(10..12), wage_lower_bound: rand(7..9), is_hidden: "false",
+      company: "test", category: "#{i}", location: "#{j}",
+      contact_email: "test@te.op", user_id: "1"])
+  end
+end
+puts "56 Public jobs created."
